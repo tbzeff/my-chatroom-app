b593b049388636773c32c3071ecc50fb34fc085f
tbzeff <taylorbleizeffer@gmail.com>
2025-08-13 01:09:39 -0600

    Hopefully fixing username emission when username has already been set in local storage

--- src/components/Chatroom.tsx
+++ src/components/Chatroom.tsx
@@ -49,12 +49,18 @@ export default function Chatroom() {
   };
 
   // Emit username if already set (e.g. on reload)
-  useEffect(() => {
-    if (isUsernameSet && username.trim()) {
+ useEffect(() => {
+  if (isUsernameSet && username.trim()) {
+    if (socket.connected) {
       socket.emit("set username", username);
+    } else {
+      socket.on("connect", () => {
+        socket.emit("set username", username);
+      });
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isUsernameSet]);
+  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [isUsernameSet, username]);
 
   const scrollToBottom = () => {
     chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
